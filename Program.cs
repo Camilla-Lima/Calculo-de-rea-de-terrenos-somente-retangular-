d931a148e2de94643611911b0bd5417fca63cfb5
Camilla-Lima <126366940+Camilla-Lima@users.noreply.github.com>
2023-02-26 21:32:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelloWorldApp {
+    class Geeks {
+        static void Main(string[] args) {
+
+            double largura = 0;
+            double comprimento = 0;
+            double area = 0;
+
+            Console.WriteLine("Calcular a area do terreno");
+
+            Console.WriteLine("Insira o valor da largura do terreno");
+            largura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor do comprimento do terreno");
+            comprimento = Convert.ToDouble(Console.ReadLine());
+
+            if (largura == comprimento){
+                Console.WriteLine("Erro: Terreno nao retangular");
+            }
+           
+           else {
+            area = largura * comprimento;
+            Console.WriteLine("Area do terreno: " + area.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+}
